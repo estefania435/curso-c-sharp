a7fb3021ac69f2624e25fa940d02d84370088e6d
estefania435 <estefa.valencia21@gmail.com>
2020-04-06 18:37:01 -0500

    aplicacion lista de estudiantes

--- curso-1/Program.cs
+++ curso-1/Program.cs
@@ -4,9 +4,42 @@ namespace curso_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hola Mundo! :D");
+            var s1 = new Estudiantes()
+            {
+                Nombre = "stefa",
+                edad = 21,
+                calificacion = 5.0
+            };
+            var s2 = new Estudiantes()
+            {
+                Nombre = "stick",
+                edad = 28,
+                calificacion = 3.0
+            };
+            var s3 = new Estudiantes()
+            {
+                Nombre = "stevenson",
+                edad = 29,
+                calificacion = 5.0
+            };
+
+            var School = new school();
+            School.addstudent(s1);
+            School.addstudent(s2);
+            School.addstudent(s3);
+            bool valor = false;
+
+            do
+            {
+                Console.WriteLine("ingrese el nombre");
+                string name = Console.ReadLine();
+                valor = School.buscarpornombre(name);
+
+            } while (valor);
+
+            
             Console.ReadKey();
         }
     }
