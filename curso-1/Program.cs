abdd43e67a3965be4ddca74ad334a46ba72d0d10
estefania435 <estefa.valencia21@gmail.com>
2020-04-06 16:08:25 -0500

    mi primer commit

---
+++ curso-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace curso_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
