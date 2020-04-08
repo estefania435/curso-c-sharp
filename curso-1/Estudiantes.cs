4e126d85ac6933e9fa2f09260581f7ebfe420093
estefania435 <estefa.valencia21@gmail.com>
2020-04-08 13:00:26 -0500

    clases

---
+++ curso-1/Estudiantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace curso_1
+{
+     public class Estudiantes
+    {
+
+        private string nombre = "stefa";
+        public int edad { get; set; }
+        public string Nombre { get; set; }
+        public double  calificacion { get; set; }
+        
+         
+        
+    }
+}
---
+++ curso-1/school.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace curso_1
+{
+    public class school
+    {
+        private List<Estudiantes> students;
+
+        public school()
+        {
+            students = new List<Estudiantes>();
+        }
+        public void addstudent(Estudiantes nuevoEstudiantes)
+        {
+            students.Add(nuevoEstudiantes);
+        }
+        public bool buscarpornombre(string name)
+        {
+            bool encontrado = false;
+            int i = 0;
+            while (encontrado == false && i < students.Count)
+            {
+                if (students[i].Nombre.Equals(name))
+                {
+                    encontrado = true;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (encontrado)
+            {
+                Console.WriteLine("name: " + students[i].Nombre + "\n"
+                    + "edad: " + students[i].edad + "\n"
+                    + "calificacion: " + students[i].calificacion);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("no existe el nombre, intente nuevamente");
+                return true;
+
+            }
+        }
+    }
+}
